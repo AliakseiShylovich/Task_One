198bd1bb5048f5e281d7007ed23f67bf3eda56f5
AliakseiShylovich <131357751+AliakseiShylovich@users.noreply.github.com>
2023-05-21 16:30:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array;
+        int numberOff = 3;                          // хранит число которое необходимо исключить из суммы
+        int sum = 0;
+        array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);  // заполняеи массив случайными числами от 0 до 10
+            System.out.print(array[i] + " ");
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == numberOff) {
+                continue;
+            }
+            sum = sum + array[i];
+        }
+        System.out.println();
+        System.out.println(sum);
+    }
+}
